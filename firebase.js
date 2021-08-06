776b44ac1788fbe6e05a1ac9f27a0a2c207a0e15
VaagnDavtyan <davtyanvaagn@gmail.com>
2021-08-06 11:25:56 -0700

    amazon

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaOPFqeFgQ_ZTJ9sSEbbzD9J_xxF0jGMk",
+    authDomain: "clone-2-e39a0.firebaseapp.com",
+    projectId: "clone-2-e39a0",
+    storageBucket: "clone-2-e39a0.appspot.com",
+    messagingSenderId: "397584307865",
+    appId: "1:397584307865:web:e4be28973da41305648a08"
+  };
---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import basketReducer from "../slices/basketSlice";
+//the global store 
+export const store = configureStore({
+  reducer: {
+    basket: basketReducer,
+  },
+});
